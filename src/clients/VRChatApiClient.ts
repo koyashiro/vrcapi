6f9223b7ca25384565d2d6def7ae06aba546e872
Koyashiro Kohaku <develop@koyashiro.net>
2021-03-23 21:39:38 +0900

    refactor: remove getter

--- src/clients/VRChatApiClient.ts
+++ src/clients/VRChatApiClient.ts
@@ -2,30 +2,17 @@ import { UserApiClient } from "./UserApiClient.ts";
 import { ApiRepository } from "../repositories/ApiRepository.ts";
 
 export class VRChatApiClient {
-  private readonly _apiKey: string;
-  private readonly _authToken: string;
-  private readonly _user: UserApiClient;
+  public readonly apiKey: string;
+  public readonly authToken: string;
+  public readonly user: UserApiClient;
 
   constructor(
     apiKey: string,
     authToken: string,
     repository: ApiRepository,
   ) {
-    this._apiKey = apiKey;
-    this._authToken = authToken;
-
-    this._user = new UserApiClient(apiKey, authToken, repository);
-  }
-
-  public get apiKey() {
-    return this._apiKey;
-  }
-
-  public get authToken() {
-    return this._authToken;
-  }
-
-  public get user() {
-    return this._user;
+    this.apiKey = apiKey;
+    this.authToken = authToken;
+    this.user = new UserApiClient(apiKey, authToken, repository);
   }
 }
