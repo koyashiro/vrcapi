98e5132581aa15ff43a907ec0d8dd5d178741be2
Koyashiro Kohaku <develop@koyashiro.net>
2021-03-24 21:19:52 +0900

    feat: add environment function

--- util.ts
+++ util.ts
@@ -1,9 +1,22 @@
 export const BASE_URL = "https://vrchat.com/api/1/";
 
-export function getAuthTokenFromEnv() {
-  const authToken = Deno.env.get("VRC_API_AUTH_TOKEN");
+export enum Environment {
+  ApiKey = "VRCAPI_API_KEY",
+  AuthToken = "VRCAPI_AUTH_TOKEN",
+}
+
+export function getApiKeyFromEnv() {
+  const authToken = Deno.env.get(Environment.ApiKey);
   if (!authToken) {
-    throw new Error("`$VRC_API_AUTH_TOKEN` is not exists.");
+    throw new Error(`$\`$${Environment.ApiKey}\` is not exists.`);
+  }
+  return authToken;
+}
+
+export function getAuthTokenFromEnv() {
+  const authToken = Deno.env.get(Environment.AuthToken);
+  if (!authToken) {
+    throw new Error(`$\`$${Environment.AuthToken}\` is not exists.`);
   }
   return authToken;
 }
