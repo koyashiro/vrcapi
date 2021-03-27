190073136edba99a03ce5e02073b60eafc334d7c
Koyashiro Kohaku <develop@koyashiro.net>
2021-03-27 14:19:49 +0900

    test: add `config_test.ts`

---
+++ config_test.ts
@@ -0,0 +1,17 @@
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.91.0/testing/asserts.ts";
+import { ConfigApiClient } from "./config.ts";
+import { VRChatApiRepository } from "./repository.ts";
+import { BASE_URL } from "./util.ts";
+
+const API_KEY = "JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26";
+const repository = new VRChatApiRepository(BASE_URL);
+const client = new ConfigApiClient(repository);
+
+Deno.test("Get config", async () => {
+  const config = await client.getConfig();
+  assert(config);
+  assertEquals(config.clientApiKey, API_KEY);
+});
