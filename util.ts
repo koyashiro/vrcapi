7714d07bee6fe8a5b04723ef7a5e0e567d65779c
Koyashiro Kohaku <develop@koyashiro.net>
2021-03-31 01:37:51 +0900

    refactor: update `util`

--- util.ts
+++ util.ts
@@ -1,10 +1,10 @@
 import { BASE_URL } from "./common.ts";
-import { Config, ConfigApiClient } from "./config.ts";
-import { VRChatApiRepository } from "./repository.ts";
+import { Repository } from "./repository.ts";
+import { ConfigApi } from "./config.ts";
 
 export async function getApiKey(): Promise<string> {
-  const repository = new VRChatApiRepository(BASE_URL);
-  const configClient = new ConfigApiClient(repository);
-  const config = await configClient.getConfig() as Config;
+  const repository = new Repository(BASE_URL);
+  const configClient = new ConfigApi(repository);
+  const config = await configClient.getConfig();
   return config.clientApiKey;
 }
